ad8a20e0c79ceb686e3e3527c33c2c392c12d0ae
Yaroslav <gorodinskiyyar@gmail.com>
2026-03-08 22:26:46 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { quizData } from './data/quizData';
 import Home from './components/Home';
 import Quiz from './components/Quiz';
